54825153c0a559f845da2d82f4b26f5520714edc
Bonfire22 <bonniecgregory@gmail.com>
2022-09-10 18:20:17 -0600

    Fixed location of Java Script Files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+<script src="js/index.js"></script>
+</html>
+
+console.log ("Your index.js file is loaded correctly!");
+
+$("myID").on ("click", function(){
+    $("div").animate({
+      height: 'toggle'
+    });
+  });
